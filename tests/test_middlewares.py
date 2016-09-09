4f2160278b48d0098e289413ea6364ed030a2064
Phil <wright.phillip@gmail.com>
2016-09-09 17:34:40 +0000

    Write tests for ssl & www redirect

--- tests/test_middlewares.py
+++ tests/test_middlewares.py
@@ -1,14 +1,15 @@
 from datetime import datetime
 from testfixtures import LogCapture
 
-from django.test import TestCase
+from django.test import TestCase, override_settings
+from django_webtest import WebTest
 from django.conf import settings
 from django.contrib.auth.models import User
 
 from library.models import RequestLog
 
-
-class MiddlewaresTestCase(TestCase):
+# class MiddlewaresTestCase(TestCase):
+class MiddlewaresTestCase(WebTest):
 
     def setUp(self):
         super(MiddlewaresTestCase, self).setUp()
@@ -16,16 +17,19 @@ class MiddlewaresTestCase(TestCase):
             username='User', password='abc123', is_staff=True, is_superuser=True)
 
     def tearDown(self):
+        print('{} RequestLogs'.format(RequestLog.objects.count()))
         RequestLog.objects.all().delete()
 
-    def test_request_login_middleware(self):
+    @override_settings(USE_WWW=False)
+    def test_request_logging_middleware(self):
         """Should create an instance of RequestLog for each request attended"""
         # Preconditions
         self.assertEqual(RequestLog.objects.count(), 0)
-
+        
         self.client.login(username=self.user.username, password='abc123')
         params = {'foo': 'bar'}
-        self.client.get('/admin/', params, follow=True)
+        self.client.get('/admin/', params, secure=True, follow=True)
+        # self.client.get('/admin/', params, follow=True)
 
         # Postconditions
         self.assertEqual(RequestLog.objects.count(), 1)
@@ -40,3 +44,97 @@ class MiddlewaresTestCase(TestCase):
         self.assertEqual(log.query_count, 0)
         self.assertTrue(isinstance(log.timestamp, datetime))
         self.assertTrue(isinstance(log.duration_in_seconds, int))
+    
+    @override_settings(USE_WWW=False)
+    def test_ssl_redirect_middleware(self):
+        """Should redirect any request not using HTTPS to the same URL over HTTPS"""
+        # Preconditions
+        self.assertEqual(RequestLog.objects.count(), 0)
+        
+        self.client.login(username=self.user.username, password='abc123')
+        response = self.client.get('/admin/', secure=False, follow=True)
+        
+        # Postconditions
+        self.assertEqual(RequestLog.objects.count(), 2)
+        first_log = RequestLog.objects.first()
+        last_log = RequestLog.objects.last()
+        self.assertEqual(first_log.scheme, 'http')
+        self.assertEqual(last_log.scheme, 'https')
+        self.assertEqual(first_log.code, 302)
+        self.assertEqual(first_log.full_url, last_log.full_url)
+    
+    @override_settings(USE_WWW=True)
+    def test_www_redirect_with_use_www(self):
+        """
+        Should redirect any request without the www subdomain to the same url
+        with the www subdomain
+        """
+        # Preconditions
+        self.assertEqual(RequestLog.objects.count(), 0)
+        
+        self.client.login(username=self.user.username, password='abc123')
+        
+        self.client.get(
+            '/admin/', 
+            SERVER_NAME='testserver', # http://stackoverflow.com/a/6291428/3697120
+            secure=True,
+            follow=True
+        )
+        
+        # Postconditions
+        self.assertEqual(RequestLog.objects.count(), 2)
+        first_log = RequestLog.objects.first()
+        last_log = RequestLog.objects.last()
+        self.assertEqual(first_log.code, 302)
+        self.assertIn('www.', last_log.abs_uri)
+        self.assertEqual(first_log.full_url, last_log.full_url)
+        
+        self.client.get(
+            '/admin/',
+            SERVER_NAME="www.testserver", 
+            secure=True,
+            follow=True
+        )
+        
+        # Postconditions
+        self.assertEqual(RequestLog.objects.count(), 3)
+        last_log = RequestLog.objects.last()
+        self.assertIn('www.', last_log.abs_uri)
+    
+    @override_settings(USE_WWW=False)
+    def test_www_redirect_with_not_use_www(self):
+        """
+        Should redirect any request using the www subdomain to the same url
+        without it
+        """
+        # Preconditions
+        self.assertEqual(RequestLog.objects.count(), 0)
+        
+        self.client.login(username=self.user.username, password='abc123')
+        
+        self.client.get(
+            '/admin/',
+            SERVER_NAME="www.testserver",
+            secure=True,
+            follow=True
+        )
+        
+        # Postconditions
+        self.assertEqual(RequestLog.objects.count(), 2)
+        first_log = RequestLog.objects.first()
+        last_log = RequestLog.objects.last()
+        self.assertEqual(first_log.code, 302)
+        self.assertNotIn('www.', last_log.abs_uri)
+        self.assertEqual(first_log.full_url, last_log.full_url)
+        
+        self.client.get(
+            '/admin/',
+            SERVER_NAME="testserver",
+            secure=True,
+            follow=True
+        )
+        
+        # Postconditions
+        self.assertEqual(RequestLog.objects.count(), 3)
+        last_log = RequestLog.objects.last()
+        self.assertNotIn('www.', last_log.abs_uri)
